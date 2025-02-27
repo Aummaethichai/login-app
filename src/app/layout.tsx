9d3ebafbcc51d9a0fe235e13641e130464a5fe95
Maethichai Kobkam <maethichai.ko@inet.co.th>
2025-02-27 16:37:46 +0700

    edit SpeedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,8 +28,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased w-screen h-screen flex justify-center items-center MuiPaper-root`}
       >
-        <SpeedInsights/>
         {children}
+        <SpeedInsights/>
       </body>
     </html>
   );
